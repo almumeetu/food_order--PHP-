95a6f5c2177ad97f108609698a1cc6f9b194c0f9
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2024-05-09 10:16:09 +0600

    admin updated

---
+++ admin/index.php
@@ -0,0 +1,42 @@
+<html>
+    <head>
+        <title>Food Order Website - Home Page</title>
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+
+
+    <body>
+        <!-- Menu Section Start Here -->
+        <div class="menu text-center">
+            <div class="wrapper">
+                 <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Admin</a></li>
+                    <li><a href="#">Category</a></li>
+                    <li><a href="#">Food</a></li>
+                    <li><a href="#">Order</a></li>
+                 </ul>
+            </div>
+        </div>
+        <!-- Menu Section End Here -->
+
+        <!-- Main Content Section Start Here -->
+        <div class="main-content">
+            <div class="wrapper">
+                Main Content Goes Here
+            </div>
+        </div>
+        <!-- Main Content Section End Here -->
+
+
+        <!-- Footer Section Start Here -->
+        <div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2024 All rights reserved, Some Restaurant. Developed By <a href="#">Al Mumeetu Saikat</a></p>
+            </div>
+        </div>
+        <!-- Footer Section End Here -->
+    </body>
+</html>
+
+
