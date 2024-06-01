4ed82739c02a78e2f99eedbd7ff2b9837381bc8a
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2024-06-01 22:06:09 +0600

    hello

---
+++ admin/add-admin.php
@@ -0,0 +1,69 @@
+<!-- for Menu -->
+<?php include('partials/menu.php'); ?>
+
+<!-- Main Content Section Start Here -->
+<div class="main-content">
+    <div class="wrapper">
+        <h1>Add Admin</h1>
+    <br>
+    <br>
+        <form action="" method="POST">
+            <table class="tbl-30">
+                <tr>
+                    <td>Full Name: </td>
+                    <td>
+                        <input type="text" name="full_name" placeholder="Enter Your Name">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Username: </td>
+                    <td>
+                        <input type="text" name="username" placeholder="Your Username">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Password: </td>
+                    <td>
+                        <input type="password" name="password" placeholder="Your Password">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td colspan="2">
+                        <input type="submit" name="submit" value="Add Admin" class="btn-secondary" placeholder="Enter Your Name">
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </div>
+</div>
+<!-- Main Content Section End Here -->
+
+<!-- for footer -->
+<?php include('partials/footer.php'); ?>
+
+
+<?php 
+    //Process the Value from Form and Save it in Database.
+
+    //check weather the submit button is clicked or not
+
+    if(isset($_POST['submit'])) {
+
+        //Button Clicked
+        // echo "Button Clicked";
+
+        //1. Get the Data from form
+        $full_name = $_POST['full_name'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        //2. SQL Query to Save the data into database
+        $sql ="INSERT INTO tbl_admin SET
+        "
+
+    }
+?>
+
---
+++ admin/manage-category.php
@@ -0,0 +1,65 @@
+<!-- for Menu -->
+<?php include('partials/menu.php'); ?>
+
+<!-- Main Content Section Start Here -->
+<div class="main-content">
+    <div class="wrapper">
+        <h1>
+            Manage Category
+        </h1>
+        <br>
+        <br>
+        <br>
+        <br>
+        <!-- Button to Add Admin -->
+        <a href="#" class="btn-primary">Add Category</a>
+        <br>
+        <br>
+        <br>
+        <br>
+        <table class="tbl-full">
+            <tr>
+                <th>S.N</th>
+                <th>Full Name</th>
+                <th>Username</th>
+                <th>Actions</th>
+            </tr>
+
+            <tr>
+                <td>1. </td>
+                <td>Al Mumeetu Saikat </td>
+                <td>almumeetu</td>
+                <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-danger">Delete Admin</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>2. </td>
+                <td>Al Mumeetu Saikat </td>
+                <td>almumeetu</td>
+                <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-danger">Delete Admin</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>3. </td>
+                <td>Al Mumeetu Saikat </td>
+                <td>almumeetu</td>
+                <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-danger">Delete Admin</a>
+                </td>
+            </tr>
+        </table>
+
+        <div class="clearfix"></div>
+    </div>
+</div>
+<!-- Main Content Section End Here -->
+
+<!-- for footer -->
+<?php include('partials/footer.php'); ?>
